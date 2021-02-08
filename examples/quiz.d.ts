2ed6db25067697c852a79afc9d20a6e40d2cfaba
André Lima <me@limwa.pt>
2021-02-08 18:53:30 +0000

    docs: add quiz configuration template

---
+++ examples/quiz.d.ts
@@ -0,0 +1,41 @@
+type Quizz = {
+    language: string,
+    title: string,
+    questions: Question[]
+}
+
+type Component = Component[] | {
+    text: string | string[],
+    type?: "normal" | "italic" | "bold" | "underlined" | "strikethrough" | "inline-code" | "block-code" | "math"
+}
+
+type Question = ChoiceQuestion | TrueFalseQuestion | ShortAnswerQuestion | DragDropQuestion;
+
+// ---------------------------------
+
+type ChoiceQuestion = {
+    questionType: "single-choice" | "multiple-choice",
+    prompt: Component,
+    answers: { 
+        content: Component,
+        correct?: boolean
+    }[]
+}
+
+type TrueFalseQuestion = {
+    questionType: "true-or-false",
+    prompt: Component,
+    correct: boolean
+}
+
+type ShortAnswerQuestion = {
+    questionType: "short-answer",
+    prompt: Component,
+    correct: string
+}
+
+type DragDropQuestion = {
+    questionType: "drag-n-drop",
+    prompt: Component,
+    // TBD
+}
